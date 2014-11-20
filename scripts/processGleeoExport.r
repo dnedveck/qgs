8d4e005039e03edb90288cc28bd1a9ae1e3032d2
Derek Nedveck <dnedveck@umn.edu>
2014-11-20 17:41:38 -0600

    generic commit, no real specific progress made

--- scripts/processGleeoExport.r
+++ scripts/processGleeoExport.r
@@ -1,1 +1,16 @@
 # this script takes as input `export_all.csv` from Gleeo
+# and returns an R object ready for another script to do an analysis on it
+
+library(dplyr)
+
+setwd("~/Dropbox/proj/qgs/data/phase2/")
+
+
+# load in the data
+
+gleeo.df <- read.csv(file = "export_all.csv", header = TRUE )
+
+# add a date column
+gleeo.df$date <- gleeo.df$Start.Date
+
+
